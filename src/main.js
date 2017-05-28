48467bd26ba20d436bc022fe820467fc831d8091
Darqoo <d.smolag@gmail.com>
2017-05-28 13:41:46 +0200

    most powerful get by server

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ var u = require('./u')
 
 var xhr = require('xhr')
 
-xhr.get("./mock/data.json", function(err, resp){
-  var object = JSON.parse(resp.body)
-  //console.log(1, object.list)
-  u.renderList(object.list);
+
+xhr.get("./mock/path.json", function(err, resp) {
+  var object1 = JSON.parse(resp.body)
+  //console.log(1, object.path)
+  xhr.get(object1.path, function(error, response) {
+    var object2 = JSON.parse(response.body)
+    u.renderList(object2.list);
+  })
 })
 
 //console.log(2, object)
